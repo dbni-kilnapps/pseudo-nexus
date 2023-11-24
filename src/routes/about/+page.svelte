219db3e6daca702d9ef47859962bbb491ef13c59
KilnApps <dustinebone@gmail.com>
2023-11-24 11:45:54 -0500

    first commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<h1>
+    Welcome to dead internet!
+</h1>
+<h2>What is the "dead internet theory"?</h2>
+<p>
+    the dead internet theory suggests that the majority of internet content is created by Artificial Intelligence
+</p>
